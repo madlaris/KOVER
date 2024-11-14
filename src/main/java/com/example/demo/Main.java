d6d3819afa5871efe44438130b1d5f73e8b92f86
madlaris <ivankoverigin@gmail.com>
2024-11-14 08:15:32 +0300

    init

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("auth-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
